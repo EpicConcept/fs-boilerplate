7eb7f6eb18cdac91e3e894ac505e3fd715aa8f75
Don <certain.concept@gmail.com>
2018-11-11 13:23:31 -0500

    first commit

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,11 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.route('/')
-  .get((req, res, next) => {
-    const message = `Example App`;
-    res.send(message);
-  });
+router.route("/").get((req, res, next) => {
+  const message = `<h1>Example App</h1>`;
+  res.send(message);
+});
 
 exports.router = router;
